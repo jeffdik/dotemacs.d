46b25b6f973a389c667ba8fd45efee4ca02ec723
Jeff Dik <s450r1@gmail.com>
2011-09-30 16:48:14 -0400

    initial jad/jad-org.el

---
+++ jad/jad-org.el
@@ -0,0 +1,11 @@
+(setq org-todo-keywords (quote ((sequence "TODO(t)" "STARTED(s!)" "|" "DONE(d!/!)")
+				(sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)"))))
+
+(setq org-todo-keyword-faces (quote (("TODO" :foreground "red" :weight bold)
+ ("STARTED" :foreground "blue" :weight bold)
+ ("DONE" :foreground "forest green" :weight bold)
+ ("WAITING" :foreground "orange" :weight bold)
+ ("SOMEDAY" :foreground "magenta" :weight bold)
+ ("CANCELLED" :foreground "forest green" :weight bold))))
+
+
