cdf2c457047f40255935307dbad9f8e1ea070ee1
Jeff Dik <s450r1@gmail.com>
2011-09-30 16:47:52 -0400

    initial custom.el

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 1)
+ '(etags-table-search-up-depth 20)
+ '(mumamo-chunk-coloring 5 t)
+ '(nxml-slash-auto-complete-flag t)
+ '(org-agenda-files nil)
+ '(org-hide-leading-stars t)
+ '(org-odd-levels-only t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-hide ((((background dark)) (:foreground "gray24")))))
