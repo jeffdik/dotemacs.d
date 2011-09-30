0663146a2f857e28affa26f31d8cb645b10359f8
Jeff Dik <s450r1@gmail.com>
2011-09-30 16:47:37 -0400

    jad-packages.el: add starter-kit-js starter-kit-ruby parenface  nsis-mode ntcmd protobuf-mode

--- jad-packages.el
+++ jad-packages.el
@@ -5,12 +5,17 @@
 
 (defvar my-packages '(starter-kit
                       starter-kit-lisp
-                                  starter-kit-eshell
-                                  starter-kit-bindings
-                                  clojure-mode clojurescript-mode clojure-test-mode
-                                  markdown-mode yaml-mode tuareg
-                                  marmalade oddmuse scpaste
-                                  color-theme color-theme-zenburn))
+                      starter-kit-eshell
+                      starter-kit-bindings
+                      starter-kit-js
+                      starter-kit-ruby
+                      clojure-mode clojurescript-mode clojure-test-mode
+                      parenface
+                      markdown-mode yaml-mode tuareg
+                      marmalade oddmuse scpaste
+                      color-theme color-theme-zenburn
+                      nsis-mode ntcmd
+                      protobuf-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
